5668946f70f65531487b978e72c97a7c0eb011d9
Alexander Bessman <alexander@bessman.se>
2022-06-07 21:31:34 +0200

    Try to improve BootloaderConnection tests readability.
    Mocking more internal interfaces reduces the need to create a bunch
    of serial packets, at the expense of increased complexity.

--- test/test_connection.py
+++ test/test_connection.py
@@ -1,7 +1,7 @@
-import pytest
-
 import intelhex
 import mock_serial
+import pytest
+import pytest_mock
 
 from mcbootflash import BootloaderConnection
 from mcbootflash import (
@@ -29,21 +29,38 @@ def mock_boot(mock_serial):
     boot.close()
 
 
+HEXFILE = intelhex.IntelHex("test/test.hex")
+
+
+READ_VERSION_RETVALS = {
+    "version": 0x0102,
+    "max_packet_length": 0x0013,
+    "device_id": 0x3456,
+    "erase_size": 0x0020,
+    "write_size": 0x08,
+}
+
+
 def test_read_version(mock_boot, mock_serial):
-    expected = {
-        "version": 0x0102,
-        "max_packet_length": 0x0100,
-        "device_id": 0x3456,
-        "erase_size": 0x0800,
-        "write_size": 0x08,
-    }
     mock_serial.stub(
         receive_bytes=bytes(CommandPacket(command=BootCommand.READ_VERSION)),
         send_bytes=bytes(
-            VersionResponsePacket(command=BootCommand.READ_VERSION, **expected)
+            VersionResponsePacket(
+                command=BootCommand.READ_VERSION, **READ_VERSION_RETVALS
+            )
         ),
     )
-    assert mock_boot.read_version() == tuple(expected.values())
+    assert mock_boot.read_version() == tuple(READ_VERSION_RETVALS.values())
+
+
+def mock_read_version(*args, **kwargs):
+    return list(READ_VERSION_RETVALS.values())
+
+
+GET_MEMORY_ADDRESS_RANGE_RETVALS = {
+    "program_start": HEXFILE.segments()[0][0] // 2,
+    "program_end": HEXFILE.segments()[0][1] // 2,
+}
 
 
 def test_get_memory_address_range(mock_boot, mock_serial):
@@ -61,6 +78,10 @@ def test_get_memory_address_range(mock_boot, mock_serial):
         ),
     )
     assert mock_boot._get_memory_address_range() == tuple(expected.values())
+
+
+def mock_get_memory_address_range(*args, **kwargs):
+    return list(GET_MEMORY_ADDRESS_RANGE_RETVALS.values())
 
 
 def test_erase_flash(mock_boot, mock_serial):
@@ -193,6 +214,9 @@ def test_self_verify_fail(mock_boot, mock_serial):
         mock_boot._self_verify()
 
 
+EXPECTED_CHECKSUM = 0x10B5
+
+
 def test_get_checksum(mock_boot, mock_serial):
     address = 0x2000
     length = 0x08
@@ -209,11 +233,15 @@ def test_get_checksum(mock_boot, mock_serial):
             ChecksumPacket(
                 command=BootCommand.CALC_CHECKSUM,
                 success=BootResponseCode.SUCCESS,
-                checksum=checksum,
+                checksum=EXPECTED_CHECKSUM,
             )
         ),
     )
-    assert mock_boot._get_checksum(address, length) == checksum
+    assert mock_boot._get_checksum(address, length) == EXPECTED_CHECKSUM
+
+
+def mock_get_checksum(*args):
+    return EXPECTED_CHECKSUM
 
 
 def test_get_checksum_fail(mock_boot, mock_serial):
@@ -238,282 +266,104 @@ def test_get_checksum_fail(mock_boot, mock_serial):
         mock_boot._get_checksum(address, length)
 
 
+def mock_get_incorrect_checksum(*args):
+    return 0
+
+
 def test_calculate_checksum(mock_boot):
     mock_boot.hexfile = intelhex.IntelHex("test/test.hex")
     segments = mock_boot.hexfile.segments()
     address = segments[0][0]
     length = segments[0][1] - segments[0][0]
-    expected_checksum = 0x10B5
-    assert mock_boot._calculate_checksum(address, length) == expected_checksum
+    assert mock_boot._calculate_checksum(address, length) == EXPECTED_CHECKSUM
 
 
-def test_checksum(mock_boot, mock_serial):
-    mock_boot.hexfile = intelhex.IntelHex("test/test.hex")
-    segments = mock_boot.hexfile.segments()
-    address = segments[0][0]
-    length = segments[0][1] - segments[0][0]
-    expected_checksum = 0x10B5
-    mock_serial.stub(
-        receive_bytes=bytes(
-            CommandPacket(
-                command=BootCommand.CALC_CHECKSUM,
-                data_length=length,
-                address=address // 2,
-            )
-        ),
-        send_bytes=bytes(
-            ChecksumPacket(
-                command=BootCommand.CALC_CHECKSUM,
-                success=BootResponseCode.SUCCESS,
-                checksum=expected_checksum,
-            )
-        ),
+def mock_calculate_checksum(*args):
+    return EXPECTED_CHECKSUM
+
+
+def test_checksum(mock_boot, mocker):
+    address = 0x2000
+    length = 0x08
+    mocker.patch.object(
+        mock_boot,
+        "_get_checksum",
+        mock_get_checksum,
+    )
+    mocker.patch.object(
+        BootloaderConnection,
+        "_calculate_checksum",
+        mock_calculate_checksum,
     )
     mock_boot._checksum(address // 2, length)
 
 
-def test_checksum_fail(mock_boot, mock_serial):
-    mock_boot.hexfile = intelhex.IntelHex("test/test.hex")
-    segments = mock_boot.hexfile.segments()
-    address = segments[0][0]
-    length = segments[0][1] - segments[0][0]
-    expected_checksum = 0x10B5
-    mock_serial.stub(
-        receive_bytes=bytes(
-            CommandPacket(
-                command=BootCommand.CALC_CHECKSUM,
-                data_length=length,
-                address=address // 2,
-            )
-        ),
-        send_bytes=bytes(
-            ChecksumPacket(
-                command=BootCommand.CALC_CHECKSUM,
-                success=BootResponseCode.SUCCESS,
-                checksum=0,
-            )
-        ),
+def test_checksum_fail(mock_boot, mocker):
+    address = 0x2000
+    length = 0x08
+    mocker.patch.object(
+        BootloaderConnection,
+        "_get_checksum",
+        mock_get_incorrect_checksum,
+    )
+    mocker.patch.object(
+        BootloaderConnection,
+        "_calculate_checksum",
+        mock_calculate_checksum,
     )
     with pytest.raises(ChecksumError):
         mock_boot._checksum(address // 2, length)
 
 
-def test_flash_segment(mock_boot, mock_serial):
-    mock_boot.hexfile = intelhex.IntelHex("test/test.hex")
+def test_flash_segment(mock_boot, mocker):
+    mock_boot.hexfile = HEXFILE
     mock_boot.max_packet_length = 0x13
     mock_boot.write_size = 8
-    for addr in range(*mock_boot.hexfile.segments()[0], 8):
-        data = mock_boot.hexfile[addr : addr + 8]
-        mock_serial.stub(
-            receive_bytes=bytes(
-                CommandPacket(
-                    command=BootCommand.WRITE_FLASH,
-                    data_length=len(data),
-                    unlock_sequence=FLASH_UNLOCK_KEY,
-                    address=addr // 2,
-                )
-            )
-            + data.tobinstr(),
-            send_bytes=bytes(
-                ResponsePacket(
-                    command=BootCommand.WRITE_FLASH,
-                    success=BootResponseCode.SUCCESS,
-                )
-            ),
-        )
-        mock_serial.stub(
-            receive_bytes=bytes(
-                CommandPacket(
-                    command=BootCommand.CALC_CHECKSUM,
-                    data_length=len(data),
-                    address=addr // 2,
-                )
-            ),
-            send_bytes=bytes(
-                ChecksumPacket(
-                    command=BootCommand.CALC_CHECKSUM,
-                    success=BootResponseCode.SUCCESS,
-                    checksum=mock_boot._calculate_checksum(
-                        address=addr, length=len(data)
-                    ),
-                ),
-            ),
-        )
+    mocker.patch.object(BootloaderConnection, "_write_flash")
+    mocker.patch.object(BootloaderConnection, "_checksum")
     mock_boot._flash_segment(mock_boot.hexfile.segments()[0])
+    mock_boot._checksum.assert_called()
 
 
-def test_flash(mock_boot, mock_serial):
-    max_packet_length = 0x0013
-    erase_size = 0x0020
-    write_size = 0x08
-    mock_boot.hexfile = intelhex.IntelHex("test/test.hex")
-    mock_serial.stub(
-        receive_bytes=bytes(CommandPacket(command=BootCommand.READ_VERSION)),
-        send_bytes=bytes(
-            VersionResponsePacket(
-                command=BootCommand.READ_VERSION,
-                version=0x0102,
-                max_packet_length=max_packet_length,
-                device_id=0x3456,
-                erase_size=erase_size,
-                write_size=write_size,
-            )
-        ),
+def test_flash(mock_boot, mocker):
+    mocker.patch.object(
+        BootloaderConnection,
+        "read_version",
+        mock_read_version,
     )
-    mock_serial.stub(
-        receive_bytes=bytes(
-            CommandPacket(command=BootCommand.GET_MEMORY_ADDRESS_RANGE)
-        ),
-        send_bytes=bytes(
-            MemoryRangePacket(
-                command=BootCommand.GET_MEMORY_ADDRESS_RANGE,
-                success=BootResponseCode.SUCCESS,
-                program_start=mock_boot.hexfile.segments()[0][0] // 2,
-                program_end=mock_boot.hexfile.segments()[0][1] // 2 + 2,
-            )
-        ),
+    mocker.patch.object(
+        BootloaderConnection,
+        "_get_memory_address_range",
+        mock_get_memory_address_range,
     )
-    mock_serial.stub(
-        receive_bytes=bytes(
-            CommandPacket(
-                command=BootCommand.ERASE_FLASH,
-                data_length=(
-                    (
-                        mock_boot.hexfile.segments()[0][1] // 2
-                        - mock_boot.hexfile.segments()[0][0] // 2
-                    )
-                    // erase_size
-                ),
-                unlock_sequence=FLASH_UNLOCK_KEY,
-                address=mock_boot.hexfile.segments()[0][0] // 2,
-            )
-        ),
-        send_bytes=bytes(
-            ResponsePacket(
-                command=BootCommand.ERASE_FLASH,
-                success=BootResponseCode.SUCCESS,
-            )
-        ),
-    )
+    mocker.patch.object(BootloaderConnection, "_erase_flash")
+    mocker.patch.object(BootloaderConnection, "_flash_segment")
+    mocker.patch.object(BootloaderConnection, "_checksum")
+    mocker.patch.object(BootloaderConnection, "_self_verify")
 
-    for addr in range(*mock_boot.hexfile.segments()[0], write_size):
-        data = mock_boot.hexfile[addr : addr + write_size]
-        mock_serial.stub(
-            receive_bytes=bytes(
-                CommandPacket(
-                    command=BootCommand.WRITE_FLASH,
-                    data_length=len(data),
-                    unlock_sequence=FLASH_UNLOCK_KEY,
-                    address=addr // 2,
-                )
-            )
-            + data.tobinstr(),
-            send_bytes=bytes(
-                ResponsePacket(
-                    command=BootCommand.WRITE_FLASH,
-                    success=BootResponseCode.SUCCESS,
-                )
-            ),
-        )
-        mock_serial.stub(
-            receive_bytes=bytes(
-                CommandPacket(
-                    command=BootCommand.CALC_CHECKSUM,
-                    data_length=len(data),
-                    address=addr // 2,
-                )
-            ),
-            send_bytes=bytes(
-                ChecksumPacket(
-                    command=BootCommand.CALC_CHECKSUM,
-                    success=BootResponseCode.SUCCESS,
-                    checksum=mock_boot._calculate_checksum(
-                        address=addr, length=len(data)
-                    ),
-                ),
-            ),
-        )
-
-    mock_serial.stub(
-        receive_bytes=bytes(
-            CommandPacket(
-                command=BootCommand.SELF_VERIFY,
-            )
-        ),
-        send_bytes=bytes(
-            ResponsePacket(
-                command=BootCommand.SELF_VERIFY,
-                success=BootResponseCode.SUCCESS,
-            )
-        ),
-    )
     mock_boot.flash("test/test.hex")
+    mock_boot._flash_segment.assert_called()
 
 
-def test_flash_out_of_range(mock_boot, mock_serial):
-    max_packet_length = 0x0013
-    erase_size = 0x0020
-    write_size = 0x08
-    mock_boot.hexfile = intelhex.IntelHex("test/test.hex")
-    mock_serial.stub(
-        receive_bytes=bytes(CommandPacket(command=BootCommand.READ_VERSION)),
-        send_bytes=bytes(
-            VersionResponsePacket(
-                command=BootCommand.READ_VERSION,
-                version=0x0102,
-                max_packet_length=max_packet_length,
-                device_id=0x3456,
-                erase_size=erase_size,
-                write_size=write_size,
-            )
-        ),
+def test_flash_out_of_range(mock_boot, mocker):
+    mocker.patch.object(
+        BootloaderConnection,
+        "read_version",
+        mock_read_version,
     )
-    mock_serial.stub(
-        receive_bytes=bytes(
-            CommandPacket(command=BootCommand.GET_MEMORY_ADDRESS_RANGE)
-        ),
-        send_bytes=bytes(
-            MemoryRangePacket(
-                command=BootCommand.GET_MEMORY_ADDRESS_RANGE,
-                success=BootResponseCode.SUCCESS,
-                program_start=mock_boot.hexfile.segments()[0][0] // 2,
-                program_end=mock_boot.hexfile.segments()[0][1] // 2,
-            )
-        ),
+
+    def mock_get_memory_address_range_illegal(*args):
+        return HEXFILE.segments()[0][0], HEXFILE.segments()[0][1]
+
+    mocker.patch.object(
+        BootloaderConnection,
+        "_get_memory_address_range",
+        mock_get_memory_address_range_illegal,
     )
-    mock_serial.stub(
-        receive_bytes=bytes(
-            CommandPacket(
-                command=BootCommand.ERASE_FLASH,
-                data_length=(
-                    (
-                        mock_boot.hexfile.segments()[0][1] // 2
-                        - mock_boot.hexfile.segments()[0][0] // 2
-                    )
-                    // erase_size
-                ),
-                unlock_sequence=FLASH_UNLOCK_KEY,
-                address=mock_boot.hexfile.segments()[0][0] // 2,
-            )
-        ),
-        send_bytes=bytes(
-            ResponsePacket(
-                command=BootCommand.ERASE_FLASH,
-                success=BootResponseCode.SUCCESS,
-            )
-        ),
-    )
-    mock_serial.stub(
-        receive_bytes=bytes(
-            CommandPacket(
-                command=BootCommand.SELF_VERIFY,
-            )
-        ),
-        send_bytes=bytes(
-            ResponsePacket(
-                command=BootCommand.SELF_VERIFY,
-                success=BootResponseCode.SUCCESS,
-            )
-        ),
-    )
+
+    mocker.patch.object(BootloaderConnection, "_erase_flash")
+    mocker.patch.object(BootloaderConnection, "_flash_segment")
+    mocker.patch.object(BootloaderConnection, "_self_verify")
+
     mock_boot.flash("test/test.hex")
+    mock_boot._flash_segment.assert_not_called()
