fd38d4c32875aac6bbdc13edf67d9b60b9add33a
Alexander Bessman <alexander@bessman.se>
2023-11-09 15:59:31 +0100

    Update tests

--- tests/test_mcbootflash.py
+++ tests/test_mcbootflash.py
@@ -6,8 +6,9 @@ import bincopy
 import pytest
 from serial import Serial
 
-import mcbootflash.__main__ as mcmain
-import mcbootflash.flash as mcbf
+import mcbootflash as bf
+import mcbootflash.__main__ as main
+import mcbootflash.flash
 import mcbootflash.protocol
 
 PORTNAME = "/dev/ttyUSB0"
@@ -32,9 +33,9 @@ def test_wrong_packet():
 )
 def test_cli(reserial, caplog, verbose, quiet):
     caplog.set_level(logging.INFO)
-    mcmain.main(
+    main.main(
         argparse.Namespace(
-            file="tests/testcases/flash/test.hex",
+            hexfile="tests/testcases/flash/test.hex",
             port=PORTNAME,
             baudrate=BAUDRATE,
             timeout=1,
@@ -47,9 +48,9 @@ def test_cli(reserial, caplog, verbose, quiet):
 
 def test_cli_error(reserial, caplog):
     caplog.set_level(logging.INFO)
-    mcmain.main(
+    main.main(
         argparse.Namespace(
-            file="tests/testcases/flash/test.hex",
+            hexfile="tests/testcases/flash/test.hex",
             port=PORTNAME,
             baudrate=BAUDRATE,
             timeout=1,
@@ -61,7 +62,7 @@ def test_cli_error(reserial, caplog):
 
 
 def test_get_parser():
-    parser = mcmain.get_parser()
+    parser = main.get_parser()
     assert parser.description == (
         "Flash firmware over serial connection to a device "
         "running Microchip's 16-bit bootloader."
@@ -69,11 +70,11 @@ def test_get_parser():
 
 
 def test_datasize_large():
-    assert mcmain.get_datasize(2**20) == "1.0 MiB"
+    assert main.get_datasize(2**20) == "1.0 MiB"
 
 
 def test_progressbar(capsys):
-    mcmain.print_progress(100, 200, 5)
+    main.print_progress(100, 200, 5)
     expected = (
         "50%  100 B  |#####################                      |  "
         "Elapsed Time: 0:00:05\r"
@@ -82,7 +83,7 @@ def test_progressbar(capsys):
 
 
 def test_get_bootattrs(reserial, connection):
-    assert mcbf.get_boot_attrs(connection) == mcbf.BootAttrs(
+    assert bf.get_boot_attrs(connection) == bf.BootAttrs(
         version=258,
         max_packet_length=256,
         device_id=13398,
@@ -96,26 +97,26 @@ def test_get_bootattrs(reserial, connection):
 def test_erase(reserial, caplog, connection):
     # To record data for this test, connect a device with a program installed.
     caplog.set_level(logging.INFO)
-    bootattrs = mcbf.get_boot_attrs(connection)
+    bootattrs = bf.get_boot_attrs(connection)
     connection.timeout = 10
-    mcmain.erase(connection, bootattrs.memory_range, bootattrs.erase_size)
+    main.erase(connection, bootattrs.memory_range, bootattrs.erase_size)
     assert "Application erased" in caplog.messages[-1]
 
 
 def test_erase_empty(reserial, caplog, connection):
     # To record data for this test, connect a device with no program installed.
     caplog.set_level(logging.INFO)
-    bootattrs = mcbf.get_boot_attrs(connection)
-    mcmain.erase(connection, bootattrs.memory_range, bootattrs.erase_size)
+    bootattrs = bf.get_boot_attrs(connection)
+    main.erase(connection, bootattrs.memory_range, bootattrs.erase_size)
     assert "No application detected, skipping erase" in caplog.messages[-1]
 
 
 def test_erase_fail(reserial, connection):
     # To record data for this test, connect a device with a program installed.
-    bootattrs = mcbf.get_boot_attrs(connection)
-    mcbf._FLASH_UNLOCK_KEY = 0
-    with pytest.raises(mcbf.BootloaderError) as excinfo:
-        mcmain.erase(
+    bootattrs = bf.get_boot_attrs(connection)
+    mcbootflash.flash._FLASH_UNLOCK_KEY = 0
+    with pytest.raises(bf.BootloaderError) as excinfo:
+        main.erase(
             connection,
             bootattrs.memory_range,
             bootattrs.erase_size,
@@ -125,30 +126,30 @@ def test_erase_fail(reserial, connection):
 
 def test_erase_misaligned():
     with pytest.raises(ValueError) as excinfo:
-        mcbf.erase_flash(Serial(), (0, 1), 2)
+        bf.erase_flash(Serial(), (0, 1), 2)
     assert "not a multiple" in str(excinfo.value)
 
 
 def test_checksum_error(reserial, connection):
-    bootattrs = mcbf.get_boot_attrs(connection)
+    bootattrs = bf.get_boot_attrs(connection)
     chunk = bincopy.Segment(
         minimum_address=bootattrs.memory_range[0],
         maximum_address=bootattrs.memory_range[0] + bootattrs.write_size,
         data=bytes(bootattrs.write_size),
         word_size_bytes=1,
     )
-    with pytest.raises(mcbf.BootloaderError) as excinfo:
-        mcbf.checksum(connection, chunk)
+    with pytest.raises(bf.BootloaderError) as excinfo:
+        bf.checksum(connection, chunk)
     assert "Checksum mismatch" in str(excinfo.value)
 
 
 def test_checksum_not_supported(reserial, caplog, connection):
-    mcbf.get_boot_attrs(connection)
+    bf.get_boot_attrs(connection)
     assert "Bootloader does not support checksumming" in caplog.messages
 
 
 def test_no_data():
-    bootattrs = mcbf.BootAttrs(
+    bootattrs = bf.BootAttrs(
         version=258,
         max_packet_length=256,
         device_id=13398,
@@ -158,36 +159,36 @@ def test_no_data():
         has_checksum=True,
     )
     with pytest.raises(ValueError) as excinfo:
-        mcbf.chunked("tests/testcases/no_data/test.hex", bootattrs)
+        bf.chunked("tests/testcases/no_data/test.hex", bootattrs)
     assert "no data" in str(excinfo.value)
 
 
 def test_reset(reserial, caplog, connection):
     caplog.set_level(logging.DEBUG)
-    mcbf.reset(connection)
+    bf.reset(connection)
     assert "Device reset" in caplog.messages[-1]
 
 
 def test_unexpected_response(reserial, connection):
-    with pytest.raises(mcbf.BootloaderError) as excinfo:
-        mcbf.reset(connection)
+    with pytest.raises(bf.BootloaderError) as excinfo:
+        bf.reset(connection)
     assert "Command code mismatch" in str(excinfo.value)
 
 
 def test_verify_fail(reserial, connection):
-    with pytest.raises(mcbf.VerifyFail):
-        mcmain.verify(connection)
+    with pytest.raises(bf.VerifyFail):
+        bf.self_verify(connection)
 
 
 def test_read_flash():
     with pytest.raises(NotImplementedError):
-        mcbf._read_flash()
+        mcbootflash.flash._read_flash()
 
 
 def test_format_debug_bytes():
     testbytes_tx = b"0123"
     testbytes_rx = b"456789"
-    formatted_tx = mcbf._format_debug_bytes(testbytes_tx)
-    formatted_rx = mcbf._format_debug_bytes(testbytes_rx, testbytes_tx)
+    formatted_tx = mcbootflash.flash._format_debug_bytes(testbytes_tx)
+    formatted_rx = mcbootflash.flash._format_debug_bytes(testbytes_rx, testbytes_tx)
     expected = "30 31 32 33\n            34 35 36 37 38 39"
     assert formatted_tx + "\n" + formatted_rx == expected
