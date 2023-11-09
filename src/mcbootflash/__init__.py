f02b7c613e4dd8f2410a69e2b785e81b883ee92c
Alexander Bessman <alexander@bessman.se>
2023-11-09 16:07:48 +0100

    Deployed a6437f6 with MkDocs version: 1.5.3

--- src/mcbootflash/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Flash firmware to devices running Microchip's 16-bit bootloader."""
-from .connection import Bootloader
-from .error import BootloaderError
-from .flashing import flash, get_parser
-
-__all__ = ["Bootloader", "BootloaderError", "flash", "get_parser"]
-
-__version__ = "7.0.7"
